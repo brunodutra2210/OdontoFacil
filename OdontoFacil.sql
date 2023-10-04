40061a6f31973f5b3b4c8243733b6520bb238a25
Luiz Psidonik <47815899+syg-luizpsidonik@users.noreply.github.com>
2023-10-04 16:42:40 -0300

    first commit

---
+++ OdontoFacil.sql
@@ -0,0 +1,16 @@
+ALTER TABLE "tb_servico" DROP CONSTRAINT IF EXISTS "tb_servico_fk0";
+
+ALTER TABLE "tb_profissional_servico" DROP CONSTRAINT IF EXISTS "tb_profissional_servico_fk0";
+
+ALTER TABLE "tb_profissional_servico" DROP CONSTRAINT IF EXISTS "tb_profissional_servico_fk1";
+
+ALTER TABLE "tb_empresa" DROP CONSTRAINT IF EXISTS "tb_empresa_fk0";
+
+ALTER TABLE "tb_agenda" DROP CONSTRAINT IF EXISTS "tb_agenda_fk0";
+
+ALTER TABLE "tb_agenda" DROP CONSTRAINT IF EXISTS "tb_agenda_fk1";
+
+ALTER TABLE "tb_agenda" DROP CONSTRAINT IF EXISTS "tb_agenda_fk2";
+
+ALTER TABLE "tb_horario" DROP CONSTRAINT IF EXISTS "tb_horario_fk0";
+
